95d9c8ddc05bb71bfb490c0df46809504f62f537
Himanshu Maurya <mhimanshu.338@gmail.com>
2019-12-27 03:36:19 +0530

    fixed same bugs backbutton

--- lib/Screen/LoginOrSignUp/Signup.dart
+++ lib/Screen/LoginOrSignUp/Signup.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:sellerapp/Screen/Admin.dart';
 import 'package:sellerapp/Screen/Wrapper.dart';
 import 'package:sellerapp/service/auth.dart';
 
