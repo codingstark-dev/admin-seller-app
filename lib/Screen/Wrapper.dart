898b277f85b01d0ee06d3b8e6cedfeb16927d42d
Himanshu Maurya <mhimanshu.338@gmail.com>
2020-01-11 02:38:19 +0530

    added updater

--- lib/Screen/Wrapper.dart
+++ lib/Screen/Wrapper.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:connectivity/connectivity.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:package_info/package_info.dart';
 import 'package:provider/provider.dart';
 import 'package:sellerapp/Screen/Admin.dart';
 import 'package:sellerapp/Screen/formDetails.dart';
@@ -11,6 +13,7 @@ import 'package:sellerapp/model/user.dart';
 import 'package:sellerapp/service/dbapi.dart';
 import 'package:sellerapp/service/streamfiles.dart';
 import 'LoginOrSignUp/Signup.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Wrapper extends StatefulWidget {
   @override
@@ -18,6 +21,16 @@ class Wrapper extends StatefulWidget {
 }
 
 class _WrapperState extends State<Wrapper> {
+  @override
+  void initState() {
+    try {
+      versionCheck(context);
+    } catch (e) {
+      print(e);
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
@@ -68,40 +81,6 @@ class _WrapperState extends State<Wrapper> {
                                       gravity: ToastGravity.BOTTOM);
                                 });
                               }
-                              // Timer.periodic(Duration(seconds: 5), (timer) {
-                              //   if (ConnectivityResult.none ==
-                              //   ConnectivityResult.none) {
-                              //      Fluttertoast.showToast(
-                              //       msg: "No Internet",
-                              //       gravity: ToastGravity.CENTER);
-                              //   }else{
-                              //      timer.cancel();
-                              //   }
-
-                              // });
-
-                              // Future.delayed(const Duration(seconds: 1), () {
-                              //   return Fluttertoast.showToast(
-                              //       msg: "No Internet",
-                              //       gravity: ToastGravity.CENTER);
-                              // });
-                              // Future.delayed(const Duration(seconds: 5), () {
-                              //   return Fluttertoast.showToast(
-                              //       msg: "No Internet",
-                              //       gravity: ToastGravity.CENTER);
-                              // });  Future.delayed(const Duration(seconds: 10), () {
-                              //   return Fluttertoast.showToast(
-                              //       msg: "No Internet",
-                              //       gravity: ToastGravity.CENTER);
-                              // });  Future.delayed(const Duration(seconds: 20), () {
-                              //   return Fluttertoast.showToast(
-                              //       msg: "No Internet",
-                              //       gravity: ToastGravity.CENTER);
-                              // });  Future.delayed(const Duration(seconds: 30), () {
-                              //   return Fluttertoast.showToast(
-                              //       msg: "No Internet",
-                              //       gravity: ToastGravity.CENTER);
-                              // });
                               return Scaffold(
                                 body: Center(
                                   child: Column(
@@ -153,5 +132,78 @@ class _WrapperState extends State<Wrapper> {
             }),
       );
     }
+  }
+}
+//Prompt users to update app if there is a new version available
+//Uses url_launcher package
+
+const PLAY_STORE_URL =
+    'https://play.google.com/store/apps/details?id=in.citygrow.seller';
+
+versionCheck(context) async {
+  //Get Current installed version of app
+  final PackageInfo info = await PackageInfo.fromPlatform();
+  double currentVersion = double.parse(info.version.trim().replaceAll(".", ""));
+
+  //Get Latest version info from firebase config
+  final RemoteConfig remoteConfig = await RemoteConfig.instance;
+
+  try {
+    // Using default duration to force fetching from remote server.
+    await remoteConfig.fetch(expiration: const Duration(seconds: 0));
+    await remoteConfig.activateFetched();
+    remoteConfig.getString('force_update_current_version');
+    double newVersion = double.parse(remoteConfig
+        .getString('force_update_current_version')
+        .trim()
+        .replaceAll(".", ""));
+    if (newVersion > currentVersion) {
+      _showVersionDialog(context);
+    }
+  } on FetchThrottledException catch (exception) {
+    // Fetch throttled.
+    print(exception);
+  } catch (exception) {
+    print('Unable to fetch remote config. Cached or default values will be '
+        'used');
+  }
+}
+
+//Show Dialog to force user to update
+_showVersionDialog(context) async {
+  await showDialog<String>(
+    useRootNavigator: false,
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      String title = "New Update Available";
+      String message =
+          "There is a newer version of app available please update it now. If your not going to update your app will not work!!";
+      String btnLabel = "Update Now";
+      // String btnLabelCancel = "Later";
+      
+      return AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(btnLabel),
+            onPressed: () => _launchURL(PLAY_STORE_URL),
+          ),
+          // FlatButton(
+          //   child: Text(btnLabelCancel),
+          //   onPressed: () => Navigator.pop(context),
+          // ),
+        ],
+      );
+    },
+  );
+}
+
+_launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
